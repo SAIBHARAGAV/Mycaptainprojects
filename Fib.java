5fc2df17cbe500ac2292946c0f94d3fd24051ed3
SAIBHARAGAV <102900085+SAIBHARAGAV@users.noreply.github.com>
2022-04-28 18:58:45 +0530

    Create Fib.java

---
+++ Fib.java
@@ -0,0 +1,15 @@
+class Fib{  
+public static void main(String args[])  
+{    
+ int a=0,b=1,c,i;    
+ System.out.print(a+" "+b);  
+    
+ for(i=1;i<=10;++i)
+ {    
+  c=a+b;    
+  System.out.print(" "+c);    
+  a=b;    
+  b=c;    
+ }    
+  
+}}  
